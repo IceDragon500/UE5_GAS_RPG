bfd5043e711e5e99ac5e070abdf8a4b454e92e6e
icedragon500 <493098698@qq.com>
2024-12-05 14:37:01 +0800

    12 Game Mode
    添加了一个继承自GameModeBase的 AuraGameModeBase c++类
    在此基础上创建了BP_GameMode蓝图，在里面设置了PlayerController 和 Pawn ，并且设置在地图中
    在AuraCharacter中，使用C++代码添加了摄像机臂和摄像机，并且设置了相关的属性
    同时也设置了CharacterMovement的属性
    最后调整了角色的动画蓝图，这样运行起来之后就是一个俯视角的游戏

---
+++ Source/Aura/Public/Game/AuraGameModeBase.h
@@ -0,0 +1,16 @@
+// 我自己用来学习的试作品 -- 来自icedragon500
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "AuraGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class AURA_API AAuraGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+};
