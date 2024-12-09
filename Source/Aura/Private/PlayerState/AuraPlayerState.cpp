be37f7ce41145045999e732bb71defaaaabef718
icedragon500 <493098698@qq.com>
2024-12-09 14:07:06 +0800

    18 The Player State
    在PlayerState中添加GAS的组件
    先创建一个继承自PlayerState的AuraPlayerState的C++类，并创建对应的蓝图，设置在GameMode中

---
+++ Source/Aura/Private/PlayerState/AuraPlayerState.cpp
@@ -0,0 +1,9 @@
+// 我自己用来学习的试作品 -- 来自icedragon500
+
+
+#include "PlayerState/AuraPlayerState.h"
+
+AAuraPlayerState::AAuraPlayerState()
+{
+	NetUpdateFrequency = 100.f;//多长时间（每秒）考虑这个参与者进行复制，用于确定NetUpdateTime
+}
---
+++ Source/Aura/Public/PlayerState/AuraPlayerState.h
@@ -0,0 +1,22 @@
+// 我自己用来学习的试作品 -- 来自icedragon500
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerState.h"
+#include "AuraPlayerState.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class AURA_API AAuraPlayerState : public APlayerState
+{
+	GENERATED_BODY()
+
+public:
+	AAuraPlayerState();
+	
+protected:
+private:
+};
